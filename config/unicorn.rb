6f7268aa4fc0cd95a68b785bf1b3ce4e62c14568
daniel-leal <danielleal94@gmail.com>
2014-04-24 13:44:39 -0300

    configuracao do unicorn para o camarao servidor

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
-working_directory "/home/leal/site/fabsoft" # Local do projeto
-pid "/home/leal/site/fabsoft/tmp/pids/server.pid" #Local do arquivo pid
-stderr_path "/home/leal/site/fabsoft/log/unicorn.stderr.log" # Arquivo de log de Erro
-stdout_path "/home/leal/site/fabsoft/log/unicorn.stdout.log" # Arquivo do log de saida
-listen "/home/leal/site/fabsoft/tmp/unicorn.todo.sock" # Guarde Esse Nome* 
+working_directory "/home/fabsoft/site/fabsoft" # Local do projeto
+pid "/home/fabsoft/site/fabsoft/tmp/pids/server.pid" #Local do arquivo pid
+stderr_path "/home/fabsoft/site/fabsoft/log/unicorn.stderr.log" # Arquivo de log de Erro
+stdout_path "/home/fabsoft/site/fabsoft/log/unicorn.stdout.log" # Arquivo do log de saida
+listen "/home/fabsoft/site/fabsoft/tmp/unicorn.todo.sock" # Guarde Esse Nome* 
 worker_processes 2
 timeout 30 # Tempo de espera até entrar em timeout(pagina demorou muito para responder)
